af462582e1f5b0213ab4e733cf76df8d684f64d4
Jakepps <nagalevsiy@mail.ru>
2023-09-28 21:41:51 +0300

    +изменение размера доски

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ document.addEventListener("DOMContentLoaded", function () {
     const status = document.getElementById("status");
     const resetButton = document.getElementById("reset-button");
 
-    // Функция для инициализации доски
     function initializeBoard() {
-        for (let i = 0; i < 25; i++) {
+        const boardSizeSelect = document.getElementById("board-size");
+        const selectedSize = parseInt(boardSizeSelect.value, 10);
+
+        board.innerHTML = '';
+
+        const gridSize = Math.sqrt(selectedSize);
+        board.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+
+        for (let i = 0; i < selectedSize; i++) {
             const cell = document.createElement("div");
             cell.classList.add("grid-cell");
             cell.setAttribute("data-index", i);
@@ -15,7 +22,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
     initializeBoard();
 
-    // Функция для обработки щелчка по ячейке
+    // Обработчик изменения выбора размера доски
+    function handleBoardSizeChange() {
+        initializeBoard();
+    }
+
+    // Добавьте слушатель события для выпадающего списка
+    const boardSizeSelect = document.getElementById("board-size");
+    boardSizeSelect.addEventListener("change", handleBoardSizeChange);
+
+    // Инициализируем доску при загрузке страницы
+    initializeBoard();
+
     function handleCellClick(event) {
         const cell = event.target;
         const index = cell.getAttribute("data-index");
@@ -30,7 +48,6 @@ document.addEventListener("DOMContentLoaded", function () {
         status.textContent = message;
     }
 
-    // Функция перезагрузки игры
     function resetGame() {
         const cells = document.querySelectorAll(".grid-cell");
         cells.forEach(function (cell) {
