b0a7dacc56da222ad53226fa462a61b1b2e7d8a2
Jakepps <nagalevsiy@mail.ru>
2023-10-12 23:33:54 +0300

    как же задизайнил мама не горюй

--- script.js
+++ script.js
@@ -3,6 +3,24 @@ document.addEventListener("DOMContentLoaded", function () {
     const status = document.getElementById("status");
     const resetButton = document.getElementById("reset-button");
 
+    const gameContainer = document.getElementById("game-container");
+    const cube = document.querySelector(".Cube");
+    const transitionOverlay = document.getElementById("transition-overlay");
+
+    cube.addEventListener("click", function () {
+        setTimeout(() => {
+            cube.classList.add("hide");
+        }, 2000)
+
+        transitionOverlay.classList.add("show");
+
+        setTimeout(() => {
+            cube.style.display = "none";
+            transitionOverlay.classList.remove("show");
+            gameContainer.style.display = "block";
+        }, 2500);
+    });
+
     function initializeBoard() {
         const boardSizeSelect = document.getElementById("board-size");
         const selectedSize = parseInt(boardSizeSelect.value, 10);
@@ -38,7 +56,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const cell = event.target;
         const index = cell.getAttribute("data-index");
         cell.classList.add("zero");
-
+        // cell.classList.add("cross");
     }
 
     board.addEventListener("click", handleCellClick);
@@ -56,5 +74,6 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     resetButton.addEventListener("click", resetGame);
+
 });
 
